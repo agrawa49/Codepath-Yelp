6f209a21dc8292ed7f90b6b0b3dc0b5cf19d0ad5
agrawa49 <agrawa49@purdue.edu>
2017-02-20 23:30:31 -0500

    uploading to Github

---
+++ Yelp/BusinessesViewController.swift
@@ -0,0 +1,186 @@
+//
+//  BusinessesViewController.swift
+//  Yelp
+//
+//  Created by Akhil Agrawal on 20/02/17.
+//  Copyright © 2017 Akhil Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, FiltersViewControllerDelegate {
+    
+    var businesses: [Business]!
+    var filtered:[Business] = []
+    var searchActive = false
+    
+    var switchStates : [Int:[Int:Bool]]! = [
+        0:[0:false],
+        1:[0:false],
+        2:[0:false],
+        3:[0:false]
+    ]
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchBar: UISearchBar!
+    //var searchSettings = GithubRepoSearchSettings()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Initialize the UISearchBar
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+        
+        // Add SearchBar to the NavigationBar
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+        
+        Business.searchWithTerm(term: "Restaurants", completion: { (businesses: [Business]?, error: Error?) -> Void in
+            
+            self.businesses = businesses
+            self.tableView.reloadData()
+            if let businesses = businesses {
+                for business in businesses {
+                    print(business.name!)
+                    print(business.address!)
+                }
+            }
+            
+            }
+        )
+        
+        /* Example of Yelp search with more search options specified
+         Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+         self.businesses = businesses
+         
+         for business in businesses {
+         print(business.name!)
+         print(business.address!)
+         }
+         }
+         */
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if businesses != nil {
+            return businesses.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+        
+        cell.business = businesses[indexPath.row]
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        // Grab refernce to filtersViewController
+        let filtersViewController = navigationController.topViewController as! FiltersViewController
+        
+        filtersViewController.delegate = self
+        filtersViewController.switchStates = self.switchStates
+    }
+
+    
+    func filtersViewController(filtersViewController: FiltersViewController, didUpdateFilters: [String : AnyObject]) {
+        var filters = didUpdateFilters
+        var dealBool : Bool
+        if(filters["deals"] as? String == "on"){
+            dealBool = true
+        } else {
+            dealBool = false
+        }
+        var categories = filters["categories"] as! [String]?
+        print("categories: \(categories)")
+        Business.searchWithTerm(term: "Restaurants", sort: filters["sort"] as? YelpSortMode, categories: filters["categories"] as! [String]? , deals: dealBool) {
+            (businesses: [Business]?, error: Error?) -> Void in
+            self.businesses = businesses
+            self.tableView.reloadData()
+        }
+    }
+    
+    func filtersViewController(filtersViewController: FiltersViewController, didSaveSwitchStates: [Int:[Int:Bool]]) {
+        self.switchStates = didSaveSwitchStates
+    }
+    
+    
+
+    
+    
+}
+
+// SearchBar methods
+extension BusinessesViewController: UISearchBarDelegate {
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+    
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        searchActive = false
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("hey")
+        print(searchBar.text)
+        Business.searchWithTerm(term: searchBar.text!, completion: { (businesses: [Business]?, error: Error?) -> Void in
+            if(businesses?.count != 0){
+                self.businesses = businesses
+            }
+            
+            self.tableView.reloadData()
+            if let businesses = businesses {
+                for business in businesses {
+                    print(business.name!)
+                    print(business.address!)
+                }
+            }
+            
+            }
+        )
+ 
+    }
+    /*
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        filtered = (self.movies?.filter() { (movie: NSDictionary) -> Bool in
+            let title: NSString = movie["original_title"]! as! NSString
+            let range = title.range(of: searchText, options: NSString.CompareOptions.caseInsensitive)
+            return range.location != NSNotFound
+            })! as [NSDictionary]
+        
+        
+        if(filtered.count == 0){
+            searchActive = false;
+        } else {
+            searchActive = true;
+        }
+        self.tableView.reloadData()
+    }
+ */
+}
